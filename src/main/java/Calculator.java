323f1c90376b92962abd04d9c0aa84fb9777e5ae
Ethan Kerr <elkerr97@gmail.com>
2023-02-28 13:05:10 -0700

    Add comment.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -74,7 +74,7 @@ class Calculator {
         for (int i = 0; i < numChars; i ++){
             builder.append((char)random.nextInt());
         }
-
+        // return the id
         return builder.toString();
     }
 
